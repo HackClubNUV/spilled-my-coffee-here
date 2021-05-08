6ecf92dc88e843f37b0f0fa7197dbce43aad3b8a
nimit2801 <mnimitsavant@outlook.com>
2021-05-09 03:41:17 +0530

    Revert "refined functions and routes"
    This reverts commit f7be02993968e569d13b87c4284512ef9314876d.
    
    This revert was made to disale the workflow

--- server.js
+++ server.js
@@ -21,10 +21,11 @@ connectDB();
 const PORT = process.env.PORT || 8080;
 console.log(PORT);
 
-async function getAllUsers(req, res, next){
+// @desc To get information of all users on the database
+app.get('/users', async (req, res, next) => {
     const users = await User.find();
-    res.status(200).json({sucess: true, count: users.length ,users: users});    
-}
+    res.status(200).json({sucess: true, count: users.legth,users: users});
+});
 
 async function newuser(req, res, next){
     let user = await User.create(req.body);
@@ -37,11 +38,10 @@ async function deleteUser(req, res, next){
 }
 
 async function updatePoints(req, res, next){
-    let user = await User.findOneAndUpdate({DiscordUSerID: req.body.DiscordUSerID}, {$inc: { points: req.body.points}}, {new : true});
-    res.status(200).json({sucess: true, user: user});
+    let user = await User.findOneAndUpdate({DiscordUSerID: req.body.DiscordUSerID}, {$inc: { points: 10}}, {new : true});
+    console.log(user);
+    res.status(200).json({sucess: true, user: user})
 }
-
-app.get('/users', getAllUsers);
 
 app.post('/newuser', newuser);
 
